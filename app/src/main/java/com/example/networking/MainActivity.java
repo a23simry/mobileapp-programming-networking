f20ca873f2482b4caeaeca4a83285c329a2786b6
SimonRydberg99 <simonrydberg@hotmail.com>
2024-04-27 21:47:29 +0200

    Internet access, some code for mainActivity for recyclerview, adapter, and json and boilerplate code for RecyclerViewAdapter.java.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -15,7 +22,14 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_FILE = "mountains.json";
 
     private ArrayList<Mountain> mountainList;
+    private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+
+    ArrayList<RecyclerViewItem> items = new ArrayList<>(Arrays.asList(
+            new RecyclerViewItem("Matterhorn"),
+            new RecyclerViewItem("Mont Blanc"),
+            new RecyclerViewItem("Denali")
+    ));
 
 
 
@@ -26,7 +40,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
         new JsonFile(this, this).execute(JSON_FILE);
 
-        //adapter = findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
+        adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
 
 
     }
@@ -34,6 +56,12 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+
+        Type type = new TypeToken<List<Mountain>>() {}.getType();
+        List<Mountain> listOfMountains = gson.fromJson(json, type);
+
     }
 
 }
